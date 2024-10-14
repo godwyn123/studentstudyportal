047bb83caf10dfd3914266b59d8c1137acc6e744
godwyn123 <gtatlonghari@student.hau.edu.ph>
2024-10-14 18:49:38 +0800

    Initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,209 @@
+from django.contrib.sites import requests
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib import messages
+from django.views import generic
+import requests
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'dashboard/home.html')
+
+
+def notes(request):
+    if request.method == 'POST':
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            notes = Notes(user=request.user, title=request.POST['title'], description=request.POST['description'])
+            notes.save()
+        messages.success(request, f"{request.user.username}, Your notes have been saved.")
+    else:
+        form = NotesForm()
+    notes = Notes.objects.filter(user=request.user)
+    context = {'notes': notes, 'form': form}
+    return render(request, 'dashboard/notes.html', context)
+
+
+def delete_note(request, pk=None):
+    Notes.objects.get(id=pk).delete()
+    return redirect("notes")
+
+
+class NotesDetailView(generic.DetailView):
+    model = Notes
+
+
+def homework(request):
+    if request.method == "POST":
+        form = HomeworkForm(request.POST)
+        if form.is_valid():
+            try:
+                finished = request.POST['is_finished']
+                if finished == 'on':
+                    finished = True
+                else:
+                    finished = False
+            except:
+                finished = False
+            homeworks = Homework(
+                user=request.user,
+                subject=request.POST['subject'],
+                title=request.POST['title'],
+                description=request.POST['description'],
+                due=request.POST['due'],
+                is_finished=finished
+
+            )
+
+            homeworks.save()
+            messages.success(request, f"{request.user.username}, Your homeworks have been saved.")
+    else:
+        form = HomeworkForm()
+    homework = Homework.objects.filter(user=request.user)
+    if len(homework) == 0:
+        homework_done = True
+    else:
+        homework_done = False
+    context = {
+        'homeworks': homework,
+        'homeworks_done': homework_done,
+        'form': form,
+    }
+    return render(request, 'dashboard/homework.html', context)
+
+
+def update_homework(request, pk=None):
+    homework = Homework.objects.get(id=pk)
+    if homework.is_finished == True:
+        homework.is_finished = False
+    else:
+        homework.is_finished = True
+    homework.save()
+    return redirect('homework')
+
+
+def delete_homework(request, pk=None):
+    Homework.objects.get(id=pk).delete()
+    return redirect("homework")
+
+
+def todo(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            try:
+                finished = request.POST['is_finished']
+                if finished == 'on':
+                    finished = True
+                else:
+                    finished = False
+            except:
+                finished = False
+            todos = Todo(
+                user=request.user,
+                title=request.POST['title'],
+                is_finished=finished
+            )
+            todos.save()
+            messages.success(request, f"{request.user.username}, Your todo have been saved.")
+    else:
+        form = TodoForm()
+    todo = Todo.objects.filter(user=request.user)
+    if len(todo) == 0:
+        todos_done = True
+    else:
+        todos_done = False
+    context = {
+        'form': form,
+        'todos': todo,
+        'todos_done': todos_done
+    }
+    return render(request, "dashboard/todo.html", context)
+
+
+def update_todo(request, pk=None):
+    todo = Todo.objects.get(id=pk)
+    if todo.is_finished == True:
+        todo.is_finished = False
+    else:
+        todo.is_finished = True
+    todo.save()
+    return redirect("todo")
+
+
+def delete_todo(request, pk=None):
+    Todo.objects.get(id=pk).delete()
+    return redirect("todo")
+
+
+def books(request):
+    if request.method == "POST":
+        form = DashboardForm(request.POST)
+        text = request.POST['text']
+        url = "https://www.googleapis.com/books/v1/volumes?q="+text
+        r = requests.get(url)
+        answer = r.json()
+        result_list = []
+        for i in range(len(answer.get('items', []))):
+            volume_info = answer['items'][i]['volumeInfo']
+            image_link = volume_info.get('imageLinks')
+
+            result_dict = {
+                'title': volume_info.get('title'),
+                'subtitle': volume_info.get('subtitle'),
+                'description': volume_info.get('description'),
+                'count': volume_info.get('pageCount'),
+                'categories': volume_info.get('categories'),
+                'rating': volume_info.get('averageRating'),
+                'thumbnail': image_link.get('thumbnail') if image_link else None,
+                'preview': volume_info.get('previewLink')
+            }
+            result_list.append(result_dict)
+            context = {
+                'form': form,
+                'results': result_list
+            }
+        return render(request, 'dashboard/books.html', context)
+    else:
+        form = DashboardForm()
+        context = {'form': form}
+        return render(request, 'dashboard/books.html', context)
+
+
+def dictionary(request):
+    if request.method == "POST":
+        form = DashboardForm(request.POST)
+        text = request.POST['text']
+        url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + text
+        r = requests.get(url)
+        answer = r.json()
+
+        try:
+            phonetics = answer[0]['phonetics'][0]['text']
+            audio = answer[0]['phonetics'][0]['audio']
+            definition = answer[0]['meanings'][0]['definitions'][0]['definition']
+            example = answer[0]['meanings'][0]['definitions'][0].get('example', 'No example available.')
+            synonyms = answer[0]['meanings'][0]['definitions'][0].get('synonyms', [])
+
+            context = {
+                'form': form,
+                'input': text,
+                'phonetics': phonetics,
+                'audio': audio,
+                'definition': definition,
+                'example': example,
+                'synonyms': synonyms
+            }
+        except (IndexError, KeyError):
+            context = {
+                'form': form,
+                'input': '',
+            }
+
+        return render(request, "dashboard/dictionary.html", context)
+    else:
+        form = DashboardForm()
+        context = {'form': form}
+        return render(request, "dashboard/dictionary.html", context)
+
